2431163d30debccf8dcaead0927935d6177181b9
saydough <garpshyperx@yahoo.com>
2024-03-02 09:43:54 +0800

    Defined mode

--- Lab1/stats.py
+++ Lab1/stats.py
@@ -6,12 +6,39 @@ def mean(n):
 
 def median(n):
     a = sorted(n)
-    if(len(n) % 2 == 0):
-        return (a[len(n)/2] + a[len(n)/2 + 1])/2
-    return a[len(n)/2]
+    index = len(n)
+    if(index % 2 == 0):
+        return (a[index/2] + a[index/2 + 1])/2
+    return a[int(index/2)]
+
+def mode(n):
+    hash = dict()
+
+    if len(n) == 0:
+        return "No Mode"
+
+    for num in n:
+        if num not in hash:
+            hash[num] = 1    
+        else:
+            hash[num] += 1
+
+    mostFreq = []
+    max = 0
+    for key, values in hash.items():
+        if values > max:
+            max = values
+
+    for key, values in hash.items():
+        if values == max:
+            mostFreq.append(key)
+    return mostFreq
 
 def main():
     print("Hello World!")
+    print("Mean:", mean([1,2,3,4]))
+    print("Median:", median([1,2,3]))
+    print("Mode:", mode([1]))
 
 if __name__ == "__main__":
     main()
