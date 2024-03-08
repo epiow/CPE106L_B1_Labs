151d2306769f4d7b6f76aac20875ca77b8e7f975
saydough <garpshyperx@yahoo.com>
2024-03-09 07:51:51 +0800

    added generator_modified.py and text files

--- Lab2/generator_modified.py
+++ Lab2/generator_modified.py
@@ -1,1 +1,42 @@
-print("test")
+"""
+Program: generator.py
+Author: Abads
+Generates and displays sentences using a simple grammar
+and vocabulary.  Words are chosen at random.
+"""
+
+import random
+
+articles = open("articles.txt", "r")
+nouns = open("nouns.txt", "r")
+prepositions = open("prepositions.txt", "r")
+verbs = open("verbs.txt", "r")
+
+def sentence():
+    """Builds and returns a sentence."""
+    return nounPhrase() + " " + verbPhrase()
+
+def nounPhrase():
+    """Builds and returns a noun phrase."""
+    return random.choice(articles) + " " + random.choice(nouns)
+
+def verbPhrase():
+    """Builds and returns a verb phrase."""
+    return random.choice(verbs) + " " + nounPhrase() + " " + \
+           prepositionalPhrase()
+
+def prepositionalPhrase():
+    """Builds and returns a prepositional phrase."""
+    return random.choice(prepositions) + " " + nounPhrase()
+
+def main():
+    """Allows the user to input the number of sentences
+    to generate."""
+    number = int(input("Enter the number of sentences: "))
+    for count in range(number):
+        print(sentence())
+
+# The entry point for program execution
+if __name__ == "__main__":
+    main()
+
